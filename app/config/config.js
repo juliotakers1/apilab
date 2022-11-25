702e40a8bd49adae0c8778e62d2c83030f6e5f48
Marcos <marcos@ciclo.tech>
2022-11-25 12:15:16 -0600

    make it better2

--- app/config/config.js
+++ app/config/config.js
@@ -2,8 +2,8 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     // DB: process.env.DB || 'mongodb://localhost:27017/labapi'
     // DB: process.env.DB || 'mongodb://JVASQUEZ:jROBERTO12@0.0.0.0:27017/labapi?authSource=admin'
-        DB: process.env.DB || 'mongodb://JVASQUEZ:jROBERTO12@localhost:27017/miapp?authSource=admin'
+        // DB: process.env.DB || 'mongodb://JVASQUEZ:jROBERTO12@localhost:27017/miapp?authSource=admin'
 
 
-    // DB: process.env.DB || 'mongodb+srv://JROBERTO:jROBERTO12@cluster0.rlrqi.mongodb.net/labapi?retryWrites=true&w=majority'
+    DB: process.env.DB || 'mongodb+srv://JROBERTO:jROBERTO12@cluster0.rlrqi.mongodb.net/labapi?retryWrites=true&w=majority'
 }
